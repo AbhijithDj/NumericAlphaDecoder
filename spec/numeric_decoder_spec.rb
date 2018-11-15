f3a9f3de784fd63a4746ed02693d3e73a51795b4
Divakar <Eswar.KJA@Honeywell.com>
2018-11-16 00:19:46 +0530

    spec for 2 test added

--- spec/numeric_decoder_spec.rb
+++ spec/numeric_decoder_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe NumericDecoder do
 	it 'raises error for invalid symbols (1) in the phone' do
 		expect {NumericDecoder.decode_numeric('1')}.to raise_error(RuntimeError, "Invalid symbol '1' in the phone")
 	end
+
+	it 'convert 6686787825 phone to expected words' do
+		expect(NumericDecoder.decode_numeric('6686787825')).to match_array( [["motor", "usual"], ["noun", "struck"], ["nouns", "truck"], ["nouns", "usual"], ["onto", "struck"], "motortruck"] )
+	end
+
+	it 'convert 2282668687 phone to expected words' do
+		expect(NumericDecoder.decode_numeric('2282668687')).to match_array( [["act", "amounts"], ["act", "contour"], ["acta", "mounts"], ["bat", "amounts"], ["bat", "contour"], ["cat", "contour"], "catamounts"] )
+	end
 end
